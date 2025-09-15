24475f069955f3751dcadde1790f75af1430b07f
Steven Wallimann <steven.wallimann@numerique.gouv.fr>
2025-09-15 14:41:17 +0200

    chore: upgrade tf provider

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     outscale = {
       source  = "outscale/outscale"
-      version = "0.10.0"
+      version = "1.2.1"
     }
     cloudinit = {
       source  = "hashicorp/cloudinit"
